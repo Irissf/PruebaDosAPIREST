9cd9c83b3bb7b64e00261d438f80a61d4f290d6c
Irissf <iris.seijo@gmail.com>
2022-02-01 15:13:43 +0100

    Agregar archivos de proyecto.

---
+++ PruebaDosAPIREST/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using PruebaDosAPIREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PruebaDosAPIREST.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+     {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Zumo", Category = "Alimento", Price = 1.2M },
+            new Product { Id = 5, Name = "Peras", Category = "Alimento", Price = 0.55M }
+
+     };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
